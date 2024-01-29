d248776407cde17e7afe6871a62ad646983a5367
DESKTOP-F12VINP\User <yush@obzen.com>
2024-01-29 13:54:03 +0900

    2024.01.29 redis hash key 패턴 스캔 로직추가

--- src/main/java/com/boot/batchWork/Controller/RedisController.java
+++ src/main/java/com/boot/batchWork/Controller/RedisController.java
@@ -2,10 +2,10 @@ package com.boot.batchWork.Controller;
 
 import com.boot.batchWork.data.redis.CacheData;
 import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
 @Slf4j
 @RestController
 @RequestMapping("/redis")
@@ -65,4 +68,111 @@ public class RedisController {
         return entity;
 
     }
+
+    /***
+     * hash Data Scan
+     * @param cacheData
+     * @return
+     */
+    @PostMapping("/fieldscan")
+    public ResponseEntity<JSONObject> getScanField(@RequestBody CacheData cacheData){
+
+        ResponseEntity<JSONObject> entity = null;
+        String cache = "";
+        String field = "";
+        SetOperations<String, String> stringStringSetOperations = stringRedisClusterTemplate.opsForSet();
+        HashOperations<String, String, String> stringObjectObjectHashOperations = stringRedisClusterTemplate.opsForHash();
+        JSONObject retJobj = new JSONObject();
+        try{
+            log.info("Request CacheName = {}, Field = {}", cacheData.getCacheName(), cacheData.getField());
+            cache = cacheData.getTableName() + "#" + cacheData.getRealFlowId() + "#" + cacheData.getWorkDt();
+            field = cacheData.getField();
+
+            String data = "";
+//            Cursor<String> cursor = stringStringSetOperations.scan(cache, ScanOptions.scanOptions().match("*").count(3).build());
+            Cursor<Map.Entry<String, String>> cursor2 = stringObjectObjectHashOperations.scan(cache, ScanOptions.scanOptions().match("*").count(3).build());
+
+            while(cursor2.hasNext()){
+                Map.Entry<String, String> next = cursor2.next();
+                //log.info("key = {}, value = {}", next.getKey(), next.getKey());
+                retJobj.put(next.getKey(), next.getValue());
+//                data += next.getKey() + "#";
+            }
+
+            data = retJobj.toJSONString();
+
+//            while(cursor.hasNext()){
+//                log.info("cursor = {}",cursor.next());
+//                data += cursor.next() + "#";
+//            }
+
+            if(data == null || data.equals("")){
+                log.info("data is null");
+                data = "Data is empty";
+            }else{
+                log.info("@@@@@@@Redis get Data = {}", data);
+            }
+
+
+            entity = new ResponseEntity<JSONObject>(retJobj, HttpStatus.OK);
+
+        }catch(Exception e){
+            e.printStackTrace();
+            retJobj.put("message",e.getMessage());
+            entity = new ResponseEntity<JSONObject>(retJobj, HttpStatus.BAD_REQUEST);
+        }
+        return entity;
+    }
+    /***
+     * hash Data Scan
+     * @param cacheData
+     * @return
+     */
+    @PostMapping("/cachescan")
+    public ResponseEntity<JSONObject> getScanCache(@RequestBody CacheData cacheData) {
+        ResponseEntity<JSONObject> entity = null;
+        String cache = "";
+        String field = "";
+        SetOperations<String, String> stringStringSetOperations = stringRedisClusterTemplate.opsForSet();
+        HashOperations<String, String, String> stringObjectObjectHashOperations = stringRedisClusterTemplate.opsForHash();
+        JSONObject retJobj = new JSONObject();
+        try{
+//            log.info("Request CacheName = {}, Field = {}", cacheData.getCacheName(), cacheData.getField());
+//            cache = cacheData.getTableName() + "#" + cacheData.getRealFlowId() + "#" + cacheData.getWorkDt();
+//            field = cacheData.getField();
+            log.info("Request tableName = {}, Field = {}", cacheData.getTableName(), cacheData.getRealFlowId());
+            String findPattKey = cacheData.getTableName() + "#" + cacheData.getRealFlowId();
+            String data = "";
+            ScanOptions scanOptions = ScanOptions.scanOptions().match(findPattKey +"*").count(100).build();
+
+            Cursor<byte[]> keys = redisClusterTemplate.getConnectionFactory().getConnection().scan(scanOptions);
+            int cnt = 1;
+            while(keys.hasNext()){
+                String next = new String(keys.next());
+                log.info("No = {} , tableName = {}",Integer.toString(cnt), next);
+                retJobj.put(Integer.toString(cnt), next);
+                cnt++;
+            }
+
+            data = retJobj.toJSONString();
+
+            if(data == null || data.equals("")){
+                log.info("data is null");
+                data = "Data is empty";
+            }else{
+                log.info("@@@@@@@Redis get Data = {}", data);
+            }
+
+
+            entity = new ResponseEntity<JSONObject>(retJobj, HttpStatus.OK);
+
+        }catch(Exception e){
+            e.printStackTrace();
+            retJobj.put("message",e.getMessage());
+            entity = new ResponseEntity<JSONObject>(retJobj, HttpStatus.BAD_REQUEST);
+        }
+
+        return entity;
+    }
+
 }
